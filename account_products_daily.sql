09bb4fbc46a072cf26b10a189f43c2de3e5886b8
unprobable-x <96841819+unprobable-x@users.noreply.github.com>
2024-10-31 13:11:11 -0400

    Update account_products_daily.sql
    added active users

--- account_products_daily.sql
+++ account_products_daily.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE TABLE hubspot.account_products_daily as 
+CREATE OR REPLACE TABLE
+  hubspot.account_products_daily AS
 WITH
   dates AS (
   SELECT
@@ -64,6 +65,59 @@ WITH
     LAG(total_deals,28) OVER(PARTITION BY account_id ORDER BY date) AS total_deals_prior28d
   FROM
     hubspot.object_counts_daily ),
+  date_windows AS (
+  SELECT
+    account_id,
+    date AS period_end_date,
+    DATE_SUB(date, INTERVAL 28 DAY) AS period_start_date
+  FROM
+    accounts_and_dates ),
+  users AS (
+  SELECT
+    DATE(event_time) AS date,
+    account_id,
+    product,
+    user_id
+  FROM
+    hubspot.usage_events
+  GROUP BY
+    1,
+    2,
+    3,
+    4 ),
+  user_window AS (
+  SELECT
+    u.account_id,
+    u.product,
+    u.user_id,
+    w.period_end_date
+  FROM
+    users u
+  LEFT JOIN
+    date_windows w
+  USING
+    (account_id)
+  WHERE
+    u.date >= w.period_start_date
+    AND u.date <= w.period_end_date ),
+  users_28d AS (
+  SELECT
+    account_id,
+    product,
+    period_end_date AS date,
+    COUNT(DISTINCT user_id) AS active_users_28d
+  FROM
+    user_window
+  GROUP BY
+    1,
+    2,
+    3 ),
+  prior_users AS (
+  SELECT
+    *,
+    LAG(active_users_28d) OVER(PARTITION BY account_id, product ORDER BY date) AS active_users_prior28d
+  FROM
+    users_28d ),
   combined AS (
   SELECT
     p.date,
@@ -84,6 +138,10 @@ WITH
   END
     AS is_activated,
     p.usage_cumulative,
+    u.active_users_28d AS active_users_last28d,
+    u.active_users_prior28d,
+    u.active_users_prior28d - u.active_users_28d AS active_users_change_last28d,
+    (u.active_users_prior28d - u.active_users_28d) / u.active_users_prior28d AS active_users_perc_change_last28d,
     CASE
       WHEN usage_last28d >= 50 THEN 1
       ELSE 0
@@ -119,7 +177,11 @@ WITH
   USING
     (account_id,
       date,
-      product) )
+      product)
+  LEFT JOIN
+    prior_users u
+  USING
+    (account_id))
 SELECT
   *,
   CASE
